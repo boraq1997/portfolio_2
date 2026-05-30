e7c324ee2968eda47ca0a2520a49ed1d1433d1bc
boraq1997 <boraqnz@gmail.com>
2026-05-30 13:28:47 +0300

    2026-05-30

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/portfolio_2/',
   plugins: [
     vue(),
     tailwindcss(),
